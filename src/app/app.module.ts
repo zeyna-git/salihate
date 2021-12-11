4592858976d285a9a7d2cd7b227fbd658a788c4c
zeyna-git <zeynabeye97@gmail.com>
2021-12-11 19:05:54 +0000

    deuxieme commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailComponent } from './detail/detail.component';
+import { AjoutComponent } from './ajout/ajout.component';
+import { NavComponent } from './nav/nav.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'ajout', component: AjoutComponent },
+  { path: 'detail', component: DetailComponent },
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    DetailComponent,
+    AjoutComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
